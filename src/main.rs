4cc0f9b31b37067e02d1f989b11821792ce149bd
tadosappo <tadosappo@users.noreply.github.com>
2019-12-05 00:23:40 +0900

    Detect kinesis or not

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use log::*;
 use std::os::unix::io::AsRawFd;
 use udev;
 
 const DEVICE_UPDATE: mio::Token = mio::Token(0);
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+  env_logger::init();
+
   let ctx = udev::Context::new()?;
   let mut builder = udev::MonitorBuilder::new(&ctx)?;
   builder.match_subsystem("input")?;
@@ -24,11 +27,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for event in events.iter() {
       match event.token() {
         DEVICE_UPDATE => {
-          let event = monitor.next().unwrap();
-          println!("{}", event.event_type());
+          let event = monitor.next();
+          if event.is_none() {
+            warn!("We are no longer able to observe keyboard connections.");
+            continue;
+          }
+          let event = event.unwrap();
 
-          for property in event.device().properties() {
-            println!("{:?} == {:?}", property.name(), property.value());
+          match event.event_type() {
+            udev::EventType::Add => {
+              let device = event.device();
+              let vendor_id = device.property_value("ID_VENDOR").and_then(|x| x.to_str());
+              let model_id = device.property_value("ID_MODEL").and_then(|x| x.to_str());
+
+              if vendor_id == Some("05f3") && model_id == Some("0007") {
+                debug!("Kinesis connected")
+              }
+            }
+            _ => {}
           }
         }
         mio::Token(_) => unreachable!(),
